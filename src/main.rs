a6f54b319e3ec6efc3800d274de94564a0d81c82
walex999 <sayadapro@gmail.com>
2025-02-19 00:25:09 +0100

    invalid commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::stdout;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
@@ -10,4 +11,8 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
-}
+    input = input.trim().to_string();
+    match input{
+        _ => eprintln!("{}: command not found",input),
+    }
+}   
