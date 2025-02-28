323e47d7d7674d2c470a0eafe9653534db072546
walex999 <sayadapro@gmail.com>
2025-03-01 00:30:33 +0100

    type extended

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use core::str;
-use std::{array, io::stdout};
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn exit_parsed(input: &Vec<&str>) {
     if input.len() > 1 {
-        std::process::exit(input[1].parse::<i32>().unwrap_or(1));
+        std::process::exit(input[1].parse::<i32>().unwrap_or_else(|_| 1));
     }
     std::process::exit(1)
 }
@@ -21,13 +20,28 @@ fn echo(input: &Vec<&str>){
 
 fn r#type(input: &Vec<&str>){
     let functions: Vec<&str> = vec!["echo","exit","type"];
+    let paths: Vec<String> = std::env::var("PATH").unwrap_or_default().split(":").map(|s|{s.to_string()}).collect();
     if input.len()>1{
         for func in &input[1..]{
             if functions.contains(func){
                 println!("{} is a shell builtin",&func);
             }
             else{
-                println!("{}: not found",&func);
+                let mut found: bool = false;
+
+                for path in &paths {
+                    let file_path = std::path::Path::new(&path).join(&func);
+                    if file_path.exists() {
+                        println!("{} is {}/{}", func, path, func);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if !found {
+                    println!("{}: not found", func);
+                }            
+        
             }
         }
     } 
@@ -38,10 +52,10 @@ fn main() {
     loop {
         input.clear();
         print!("$ ");
-        io::stdout().flush().unwrap();
+        io::stdout().flush().unwrap_or_default();
 
         let stdin = io::stdin();
-        stdin.read_line(&mut input).unwrap();
+        stdin.read_line(&mut input).unwrap_or_default();
         let parsed: Vec<&str> = input.split_whitespace().collect();
         if parsed.len() > 0 {
             match parsed[0] {
