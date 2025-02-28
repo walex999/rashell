f409f1ae8886c1a01742d1ce7e3c7ee018db9fbd
walex999 <sayadapro@gmail.com>
2025-02-28 22:05:46 +0100

    echo command

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,38 @@ use std::io::stdout;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-
-fn exit_parsed(input:&String){
-    let parsed: Vec<&str> = input.split_whitespace().collect();
-    if parsed.len() > 1 {
-       // println!("exiting with code {}",parsed[1].parse::<i32>().unwrap_or(1).to_string());
-        std::process::exit(parsed[1].parse::<i32>().unwrap_or(1));
+fn exit_parsed(input: &Vec<&str>) {
+    if input.len() > 1 {
+        std::process::exit(input[1].parse::<i32>().unwrap_or(1));
     }
-    // println!("exiting with code 1 default");
-    std::process::exit(1);
+    std::process::exit(1)
+}
+
+fn echo(input: &Vec<&str>){
+    if input.len()>1{
+        for word in &input[1..] {
+            print!("{} ",word);
+        }
+    }
+    println!("");
 }
 
 fn main() {
     let mut input = String::new();
-    loop{
+    loop {
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
-        
+
         let stdin = io::stdin();
         stdin.read_line(&mut input).unwrap();
-
-        match input{
-            _ if input.starts_with("exit") => exit_parsed(&input),
-            _ => eprintln!("{}: command not found",input.trim()),
+        let parsed: Vec<&str> = input.split_whitespace().collect();
+        if parsed.len() > 0 {
+            match parsed[0] {
+                "exit" => exit_parsed(&parsed),
+                "echo" => echo(&parsed),
+                _ => eprintln!("{}: command not found", input.trim()),
+            }
         }
     }
-}   
+}
