114c76749f7aa08b6f8bd1ad734df72c6a9e65dc
walex999 <sayadapro@gmail.com>
2025-03-01 01:54:40 +0100

    running all commands

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,44 @@ fn r#type(input: &Vec<&str>){
     } 
 }
 
+fn run_command(input: &Vec<&str>){
+    let paths: Vec<String> = std::env::var("PATH").unwrap_or_default().split(":").map(|s|{s.to_string()}).collect();
+    let mut found: bool = false;
+    for path in &paths {
+        let file_path = std::path::Path::new(&path).join(&input[0]);
+        if file_path.exists()  {
+            let mut command = std::process::Command::new(file_path);
+            if input.len()> 1 {
+               command.args(&input[1..]); 
+            }
+            match command.output() {
+                Ok(output) => {
+                    found = true;
+                    // Print the command's stdout (standard output)
+                    if !output.stdout.is_empty() {
+                        print!("{}", String::from_utf8_lossy(&output.stdout));
+                    }
+
+                    // Print the command's stderr (standard error)
+                    if !output.stderr.is_empty() {
+                        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+                    }
+                },
+                Err(e) => {
+                    eprintln!("Error executing command: {}", e);
+                }
+            }
+        
+        }
+        if found ==true{
+            break;
+        }
+    }
+    if found==false{
+        eprintln!("{}: command not found", &input[0].trim());
+    }
+}
+
 fn main() {
     let mut input = String::new();
     loop {
@@ -62,7 +100,8 @@ fn main() {
                 "exit" => exit_parsed(&parsed),
                 "echo" => echo(&parsed),
                 "type" => r#type(&parsed),
-                _ => eprintln!("{}: command not found", input.trim()),
+                _ => run_command(&parsed),
+                //_ => eprintln!("{}: command not found", input.trim()),
             }
         }
     }
